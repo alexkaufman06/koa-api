6c965030bfffafd692278b9d1fcd8a49b07289ad
Kaufman <alexkaufman06@gmail.com>
2019-11-05 17:10:41 -0800

    Add koa router

--- app.js
+++ app.js
@@ -1,20 +1,12 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 const app = new Koa();
-
-app.use(async (ctx, next) => {
-  console.log('First');
-  await next();
-  console.log('First done');
-});
-
-app.use(async (ctx, next) => {
-  console.log('Second');
-  await next();
-  console.log('Second done');
-});
+const router = new Router();
 
 app.use(async (ctx) => {
   ctx.body = 'Hello it\'s me... KOA!'
 });
+
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3100);
