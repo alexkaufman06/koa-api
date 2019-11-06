8a38600608418958c8c3bf30bbea7777e2b497f2
Kaufman <alexkaufman06@gmail.com>
2019-11-06 07:39:06 -0800

    Add ejs templates

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const render = require('koa-ejs');
+const path = require('path');
 const app = new Koa();
 const router = new Router();
+
+render(app, {
+  root: path.join(__dirname, "views"),
+  layout: "template",
+  viewExt: "html",
+  cache: false,
+  debug: false,
+  async: true
+});
+
+const users = [
+  "Joe",
+  "Orin",
+  "Bob",
+  "Satchmo"
+]
+
+router.get('/users', async ctx => {
+  await ctx.render('index', {
+    users: users
+  });
+});
 
 router.get('/test/:id', (ctx) => {
   ctx.body = {
