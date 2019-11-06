9912fd14377bf0471314d107800ea39323e889a0
Kaufman <alexkaufman06@gmail.com>
2019-11-05 19:13:41 -0800

    Add get/post paths

--- app.js
+++ app.js
@@ -3,10 +3,20 @@ const Router = require('koa-router');
 const app = new Koa();
 const router = new Router();
 
-app.use(async (ctx) => {
-  ctx.body = 'Hello it\'s me... KOA!'
+router.get('/test/:id', (ctx) => {
+  ctx.body = {
+    name: 'Hello',
+    id: ctx.params.id
+  };
+});
+
+router.post('/', (ctx) => {
+  ctx.body = ctx.request.req;
+  return (ctx.status = 201);
 });
 
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(3100);
+
+// https://www.youtube.com/watch?v=LWP5j4NR-XI 21:44 ish
