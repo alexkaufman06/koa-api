2cf6e22025b287dd4913ff08bff1f7d1bc22583e
Kaufman <alexkaufman06@gmail.com>
2019-11-05 17:06:03 -0800

    Add nodemon and up/down stream example

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const Koa = require('koa');
 const app = new Koa();
 
-app.use(async (ctx) => {
-  ctx.body = "Hello it's me... KOA" // ctx is an object that encapsulates 2 objects (node request and response objects)
+app.use(async (ctx, next) => {
+  console.log('First');
+  await next();
+  console.log('First done');
 });
 
-app.listen(3000);
+app.use(async (ctx, next) => {
+  console.log('Second');
+  await next();
+  console.log('Second done');
+});
+
+app.use(async (ctx) => {
+  ctx.body = 'Hello it\'s me... KOA!'
+});
+
+app.listen(3100);
